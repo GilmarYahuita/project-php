aec249d0dcd12af8792ae7790462674025aa4785
Gilmar Yahuita <gilmaralexandreprado@gmail.com>
2024-04-08 19:34:13 -0300

    Struct Project

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    $nome = "Gilmar";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis, sint illo! Recusandae explicabo excepturi minima? Commodi deserunt possimus quibusdam reprehenderit atque harum rerum pariatur. Excepturi, dolor odit. Cumque, tempora earum?</p>
+    <p>Seu nome é <?=$nome?></p>
+</body>
+</html>
